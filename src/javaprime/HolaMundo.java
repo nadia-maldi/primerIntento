da5bd99bdfba353314f38c32e004fe7168a75c3f
Rulo <nadia.maldi@mi.unc.edu.ar>
2023-03-08 12:52:39 -0300

    primer commit

---
+++ src/javaprime/HolaMundo.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaprime;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Maldi
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner leer = new Scanner (System.in);
+        String nombre;
+        System.out.println("ingresa tu nombre");
+        nombre = leer.next();
+        System.out.println("hola mundo! soy "+ nombre + " y estoy programando en java!");
+        
+
+    }
+    
+}
